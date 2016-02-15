c8812296422d2bfbbed89fb36dae9e1e1dd35be6
alex-crisp <sashamovin@gmail.com>
2016-02-15 23:09:12 +0200

    second commit - from ide

---
+++ src/main/java/ua/com/crisp/spring_games/test.java
@@ -0,0 +1,11 @@
+package ua.com.crisp.spring_games;
+
+/**
+ * Created by crisp on 2/15/2016.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+}
